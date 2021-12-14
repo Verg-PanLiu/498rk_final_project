7d19e31ab03f0501eb1aaf322f5123182a016aaf
MarcoYLyu <myilyu19990707@gmail.com>
2021-12-13 22:34:42 -0600

    sign up finish

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,11 +1,49 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
-import RecipesList from "../components/RecipesList"
 import SEO from "../components/SEO"
 
+import axios from 'axios'
+
 const Signup = ({ data }) => {
-  // const recipes = data.allContentfulRecipe.nodes
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("MALE");
+  const [invalid, setInvalid] = useState(false);
+
+  let errMsg = "";
+  const handleSignUp = (event) => {
+    event.preventDefault();
+
+    if (password !== password2) {
+      setInvalid(true);
+      return;
+    }
+    
+    let json = {
+      UserId: 100,
+      UserName: name,
+      UserPassword: password,
+      Gender: gender,
+      EmailAddress: email
+    };
+    axios.post("http://localhost:4000/api/users/", json)
+    .then((res) => {
+      console.log(res);
+      window.location = "/login";
+    })
+    .catch((error) => {
+      console.log(error);
+      setInvalid(true);
+    });
+  };
+
+  if (invalid) {
+    errMsg = "Registration Failure";
+  }
+
   return (
     <Layout>
       <SEO title="Contact" />
@@ -13,25 +51,37 @@ const Signup = ({ data }) => {
         <section className="contact-page">
           <article className="contact-info">
             <h1>Welcome!</h1>
-            <p><h4> Thanks for joining us!</h4></p>
+            <h4> Thanks for joining us!</h4>
           </article>
           <article>
-            <form className="form contact-form">
+            <form className="form contact-form" onSubmit={handleSignUp}>
+              <div className="err-msg">
+                {errMsg}
+              </div>
+
               <div className="form-row">
-                <label className="form-label">User Name</label>
-                <input type="text" name="name" id="name" className="form-input"/>
+                <label className="form-label" htmlFor="name">User Name</label>
+                <input type="text" name="name" value={name} onChange={e => setName(e.target.value)} id="name" className="form-input" required/>
               </div>
               <div className="form-row">
-                <label className="form-label">User Password</label>
-                <input type="text" name="password1" id="password1" className="form-input"/>
+                <label className="form-label" htmlFor="password1">User Password</label>
+                <input type="text" name="password1" id="password1" value={password} onChange={e=> setPassword(e.target.value)} className="form-input" required/>
+              </div>
+
+              <div className="form-row">
+                <label className="form-label" htmlFor="password2">Repeat Password</label>
+                <input type="text" name="password2" value={password2} onChange={e=> setPassword2(e.target.value)} id="password2" className="form-input" required/>
               </div>
               <div className="form-row">
-                <label className="form-label">Repeat Password</label>
-                <input type="text" name="password2" id="password2" className="form-input"/>
+                <label className="form-label" htmlFor="email">Email</label>
+                <input type="text" name="email" id="email" value={email} onChange={e => setEmail(e.target.value)} className="form-input" repeat/>
               </div>
               <div className="form-row">
-                <label className="form-label">Email</label>
-                <input type="text" name="email" id="email" className="form-input"/>
+                <label className="form-label" htmlFor="gender">Gender</label>
+                <select className="form-input" id="gender" name = "registerGender" value={gender} onChange={e => setGender(e.target.value)}>
+                  <option value="MALE">MALE</option>
+                  <option value="FEMALE">FEMALE</option>
+                </select>
               </div>
               <button id="signup" type="submit" className="btn btn-block">
                 Create Account
